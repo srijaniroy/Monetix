0bb27fa125b3a50b50cecf41ca9f525e127efce8
srijaniroy <srijaniroy185@gmail.com>
2025-03-13 21:46:17 +0530

    Arcjet shield and bot protection

--- middleware.js
+++ middleware.js
@@ -8,7 +8,25 @@ const isProtectedRoute = createRouteMatcher([
   "/transaction(.*)",
 ]);
 
-
+// Create Arcjet middleware
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  // characteristics: ["userId"], // Track based on Clerk userId
+  rules: [
+    // Shield protection for content and security
+    shield({
+      mode: "LIVE",
+    }),
+    detectBot({
+      mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
+      allow: [
+        "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
+        "GO_HTTP", // For Inngest
+        // See the full list at https://arcjet.com/bot-list
+      ],
+    }),
+  ],
+});
 
 // Create base Clerk middleware
 const clerk = clerkMiddleware(async (auth, req) => {
